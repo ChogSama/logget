cd9494769e335f8d132810d8abbbc51969c119da
ChogSama <chogsama123@gmail.com>
2026-05-29 00:08:53 +0700

    chore(frontend): setup React + Vite

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom"
+import Test from "./pages/Test"
 
-function App() {
+export default function App() {
   return (
     <Routes>
-      
+      <Route path="/" element={
+        <button className="px-4 py-2 bg-purple-500 text-white rounded-xl">
+          Test Tailwind
+        </button>} />
+      <Route path="/test" element={<Test />} />
     </Routes>
   )
 }
-
-export default App
