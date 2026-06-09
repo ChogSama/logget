190c765a2884c1775839bc03690266443b6ca2e2
doman <ddooxmanhhiep@gmail.com>
2026-06-10 01:17:36 +0700

    chore(web): add base44 sdk deps, will remove later if possible

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from "path"
+
+import base44 from "@base44/vite-plugin"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '..', 'VITE_')
@@ -8,6 +11,15 @@ export default defineConfig(({ mode }) => {
   return {
     envDir: '..',
     plugins: [
+      base44({
+        // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
+        // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
+        legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+        hmrNotifier: true,
+        navigationNotifier: true,
+        analyticsTracker: true,
+        visualEditAgent: true
+      }),
       react(),
       VitePWA({
         registerType: 'autoUpdate',
@@ -41,6 +53,11 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     server: {
       allowedHosts: [
         '*.ngrok-free.app',
