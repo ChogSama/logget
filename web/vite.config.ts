ba11dd2148b3cf09e6a6b4fd9cca0399c81d2052
doman <ddooxmanhhiep@gmail.com>
2026-06-10 01:57:38 +0700

    chore(web): remove base44 sdk

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,23 +3,12 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import path from "path"
 
-import base44 from "@base44/vite-plugin"
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '..', 'VITE_')
 
   return {
     envDir: '..',
     plugins: [
-      base44({
-        // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-        // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-        legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-        hmrNotifier: true,
-        navigationNotifier: true,
-        analyticsTracker: true,
-        visualEditAgent: true
-      }),
       react(),
       VitePWA({
         registerType: 'autoUpdate',
